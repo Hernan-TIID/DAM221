98a509ecd039390c044d240372aacf688cc66c61
Hernan-TIID <125053767@upq.edu.mx>
2026-09-14 20:28:52 -0600

    IntroduccionJS
    Commit de introduccion a Javascript trabajando para back y para front

---
+++ IntroJS/HolaNode.js
@@ -0,0 +1,13 @@
+console.log("Hola mundo: NODE!");
+
+let edad1 = 20;
+let edad2 = 18;
+console.log("Edad promedio: ");
+console.log((edad1+edad2)/2);
+
+console.time("miProceso");
+    for(i=0;i>=1000000000000000000000000000000;i++)
+    {
+
+    }
+console.timeEnd("miProceso");
